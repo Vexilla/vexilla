b90656a68e3971e0d3e35942954a3a0ba820af47
Chris Griffing <cmgriffing@gmail.com>
2023-07-06 18:20:08 -0700

    feat: update js client to meet updated schema from app

--- clients/client-js/src/client.ts
+++ clients/client-js/src/client.ts
@@ -5,39 +5,152 @@ import {
   VexillaFeatureTypeSelective,
   VexillaFlags,
   VexillaGradualFeature,
+  VexillaManifest,
+  PublishedGroup,
 } from "@vexilla/types";
+import * as Case from "case";
 
 import Hasher from "./hasher";
 
+const LATEST_MANIFEST_VERSION = 1;
+
+type FetchHook<T> = (url: string) => Promise<T>;
+
 export class VexillaClient {
+  protected suppressLogs;
   protected baseUrl: string;
   protected environment: string;
   protected customInstanceHash = "";
 
-  protected flags: VexillaFlags;
+  protected manifest: VexillaManifest;
+  protected flagGroups: Record<string, PublishedGroup> = {};
+  protected groupLookupTable: Record<string, string> = {};
 
-  constructor(config: VexillaClientConfig) {
+  constructor(config: VexillaClientConfig, suppressLogs = false) {
+    this.suppressLogs = suppressLogs;
     this.baseUrl = config.baseUrl;
     this.environment = config.environment || "prod";
     this.customInstanceHash = config.customInstanceHash;
   }
 
-  async getFlags(fileName: string, fetchHook: (url: string) => Promise<any>) {
-    const flagsResponse: any = await fetchHook(`${this.baseUrl}/${fileName}`);
+  async getManifest(
+    fetchHook: FetchHook<VexillaManifest>
+  ): Promise<[VexillaManifest, Record<string, string>]> {
+    try {
+      const manifestResponse = await fetchHook(`${this.baseUrl}/manifest.json`);
+
+      const groupLookupTable: Record<string, string> = {};
+
+      manifestResponse.groups.forEach((_group) => {
+        const scrubbedName = _group.name.replace(".json", "");
+        groupLookupTable[_group.groupId] = _group.groupId;
+        groupLookupTable[scrubbedName] = _group.groupId;
+        groupLookupTable[Case.kebab(scrubbedName)] = _group.groupId;
+      });
+
+      return [manifestResponse, groupLookupTable];
+    } catch (e: any) {
+      return [{ version: "v0", groups: [] }, {}];
+    }
+  }
+
+  async setManifest(
+    manifest: VexillaManifest,
+    groupLookupTable: Record<string, string>
+  ) {
+    this.manifest = manifest;
+    this.groupLookupTable = groupLookupTable;
+
+    const currentVersion = this.manifest.version
+      ? parseInt(this.manifest.version.replace("v", ""))
+      : 0;
+
+    if (currentVersion !== LATEST_MANIFEST_VERSION) {
+      throw new Error(
+        `Manifest version mismatch. Current: ${currentVersion} - Required: ${LATEST_MANIFEST_VERSION}. You must either use an appropriate client or you must update your schema.`
+      );
+    }
+
+    const fetchedFlags = await Promise.all(
+      this.manifest.groups.map((group) =>
+        this.getFlags(group.name, async (url) => {
+          const response = await fetch(url);
+          return response.json();
+        })
+      )
+    );
+
+    fetchedFlags.forEach((flagGroup) => {
+      this.flagGroups[flagGroup.groupId] = flagGroup;
+    });
+  }
+
+  async syncManifest() {
+    const [manifest, groupLookupTable] = await this.getManifest(async (url) => {
+      const response = await fetch(url);
+      return response.json();
+    });
+
+    this.setManifest(manifest, groupLookupTable);
+  }
+
+  async getFlags(
+    fileName: string,
+    fetchHook: FetchHook<VexillaFlags>
+  ): Promise<PublishedGroup> {
+    let flagFileName = fileName.replace(".json", "");
+    let groupId = flagFileName;
+    if (this.manifest) {
+      groupId = this.groupLookupTable[flagFileName];
+    }
+
+    const flagsResponse: any = await fetchHook(
+      `${this.baseUrl}/${groupId}.json`
+    );
     return flagsResponse;
   }
 
-  setFlags(flags: VexillaFlags) {
-    this.flags = flags;
+  setFlags(groupId: string, flags: PublishedGroup) {
+    groupId = this.groupLookupTable[groupId];
+    this.flagGroups[groupId] = flags;
   }
 
-  should(flagName: string, groupName = "untagged") {
-    if (!this.flags) {
+  // combined getter and setter
+  async updateFlags(fileName: string, fetchHook: FetchHook<VexillaFlags>) {
+    const flags = await this.getFlags(fileName, fetchHook);
+  }
+
+  should(flagName: string, groupName: string) {
+    const groupId = this.groupLookupTable[groupName];
+    if (!this.flagGroups[groupId]) {
+      this.warn("should() called before flags were fetched for this group");
       return false;
     }
 
-    let flag =
-      this.flags["environments"]?.[this.environment]?.[groupName]?.[flagName];
+    // const environmentId =
+    const environment = this.flagGroups[groupId].environments.find(
+      (_environment) =>
+        _environment.environmentId === this.environment ||
+        _environment.name === this.environment ||
+        _environment.name === Case.kebab(this.environment)
+    );
+
+    if (!environment) {
+      console.error(
+        `Environment (${this.environment}) not found in group (${groupName}, ${groupId}).`
+      );
+
+      return false;
+    }
+
+    const feature = this.flagGroups[groupId].features.find(
+      (_feature) =>
+        _feature.featureId === flagName ||
+        _feature.name === flagName ||
+        _feature.name === Case.kebab(flagName)
+    );
+
+    let flag = environment.features[feature.featureId];
 
     if (!flag) {
       console.error(
@@ -70,7 +183,7 @@ export class VexillaClient {
           return false;
         }
         flag = flag as VexillaGradualFeature;
-        _should = this.getInstancePercentile(flag.seed) <= flag.value;
+        _should = this.getInstancePercentile(flag.seed) <= flag.value * 100;
         break;
 
       default:
@@ -80,8 +193,20 @@ export class VexillaClient {
     return _should;
   }
 
-  private getInstancePercentile(seed: number) {
+  protected getInstancePercentile(seed: number) {
     const hasher = new Hasher(seed);
     return hasher.hashString(this.customInstanceHash);
+  }
+
+  protected log(...props) {
+    if (!this.suppressLogs) {
+      console.log(...props);
+    }
+  }
+
+  protected warn(...props) {
+    if (!this.suppressLogs) {
+      console.warn(...props);
+    }
   }
 }
--- clients/client-js/src/test_client.ts
+++ clients/client-js/src/test_client.ts
@@ -1,30 +1,38 @@
+import { VexillaManifest } from "@vexilla/types";
 import { VexillaClient } from "./client";
 import axios from "axios";
 
 (async () => {
   let uuid = "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a";
+  const groupName = "Group 1";
 
   const client = new VexillaClient({
-    baseUrl:
-      "http://streamparrot-feature-flags.s3-website-us-east-1.amazonaws.com",
+    baseUrl: "http://localhost:3000",
     environment: "dev",
     customInstanceHash: uuid,
   });
 
-  const flags = await client.getFlags("features.json",async (url: string) => {
+  await client.syncManifest();
+
+  const flags = await client.getFlags(groupName, async (url: string) => {
     const response = await axios.get(url);
     return response.data;
   });
 
-  client.setFlags(flags);
+  client.setFlags(groupName, flags);
 
-  const shouldGradual = client.should("testingWorkingGradual");
-  const shouldNotGradual = client.should("testingNonWorkingGradual");
+  console.log("FLAGS", JSON.stringify(flags, null, 2));
+
+  const shouldGradual = client.should("testingWorkingGradual", groupName);
+
+  console.log({ shouldGradual });
 
   if (!shouldGradual) {
     console.error("Should Gradual, but couldn't");
     process.exit(1);
   }
+
+  const shouldNotGradual = client.should("testingNonWorkingGradual", groupName);
 
   if (shouldNotGradual) {
     console.error("Should Not Gradual, but could");
