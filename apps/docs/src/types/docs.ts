3789d041874217b7a120da56cadd3b04914e098d
Chris Griffing <cmgriffing@gmail.com>
2023-09-15 17:31:47 -0700

    feat: add guides

--- apps/docs/src/types/docs.ts
+++ apps/docs/src/types/docs.ts
@@ -3,3 +3,9 @@ export type tomlKey = "typescript" | "rust" | "go";
 export const tomlKeys: tomlKey[] = ["typescript", "rust", "go"];
 
 export type CodeSnippetContent = Record<tomlKey, string>;
+
+export interface Heading {
+  depth: number;
+  slug: string;
+  text: string;
+}
