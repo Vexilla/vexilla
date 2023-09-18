9560707c71593832d919109bca8e2868a2429b83
Chris Griffing <cmgriffing@gmail.com>
2023-09-18 15:51:10 -0700

    fix: clean up astro config

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-
 import toml from "astro-toml";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 4321,
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
