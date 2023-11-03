92531fdd5f5e967c41525145ee920466ffc10870
Chris Griffing <cmgriffing@gmail.com>
2023-11-03 14:37:02 -0700

    fix: update gradual tests

--- clients/js/src/test_gradual.ts
+++ clients/js/src/test_gradual.ts
@@ -1,14 +1,13 @@
-import Hasher from "./hasher";
+import assert from "assert";
+import { hashString } from "./hasher";
 
 let uuid = "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a";
 
 let workingSeed: number = 0.11;
 let nonWorkingSeed: number = 0.22;
 
-console.log("Working Hash:", new Hasher(workingSeed).hashString(uuid));
-console.log("WORKING SEED", new Hasher(workingSeed).hashString(uuid) <= 40);
+console.log("WORKING SEED", hashString(uuid, workingSeed));
+assert.ok(hashString(uuid, workingSeed) <= 0.4);
 
-console.log(
-  "NON WORKING SEED",
-  new Hasher(nonWorkingSeed).hashString(uuid) > 40
-);
+console.log("NON WORKING SEED", hashString(uuid, nonWorkingSeed));
+assert.ok(hashString(uuid, workingSeed) > 0.4);
