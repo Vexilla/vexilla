0db9494f92ab4abcab9a67e5b193aaaff0d918d3
Chris Griffing <cmgriffing@gmail.com>
2023-11-22 16:20:14 -0800

    fix: change path for test server in php script

--- clients/php/tests/ClientTest.php
+++ clients/php/tests/ClientTest.php
@@ -10,7 +10,7 @@ final class ClientTest extends TestCase
   public function testClient()
   {
     $client = new Client(
-      'http://host.docker.internal:3000',
+      'http://localhost:3000',
       'dev',
       $this->uuid,
       true
