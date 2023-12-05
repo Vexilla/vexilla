5bc9c70746b8d1b0479aa20aefc95bc594ab1eeb
Chris Griffing <cmgriffing@gmail.com>
2023-12-05 15:25:26 -0800

    fix: remove numberType from BaseSelectiveFeature

--- clients/python/vexilla_client/types.py
+++ clients/python/vexilla_client/types.py
@@ -110,7 +110,6 @@ ValueFeature = Annotated[
 class BaseSelectiveFeature(BaseFeature):
     feature_type: Literal[FeatureType.SELECTIVE] = Field(..., alias="featureType")
     value_type: ValueType = Field(..., alias="valueType")
-    number_type: NumberType = Field(..., alias="numberType")
 
 
 class SelectiveStringFeature(BaseSelectiveFeature):
