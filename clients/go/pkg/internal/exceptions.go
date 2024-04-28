3420048fc55b14a9e4cde570671f7e599cbc6814
Heijnen,Dirk D.J.C <djc.heijnen@student.fontys.nl>
2024-04-28 14:28:14 +0200

    Added specific typed errors for the go client

--- clients/go/pkg/internal/exceptions.go
+++ clients/go/pkg/internal/exceptions.go
@@ -2,40 +2,206 @@ package internal
 
 import "fmt"
 
-type VexillaError struct {
-	Type    string
-	Message string
-	Detail  string
+type InvalidShouldFeatureType struct {
+	Detail string
 }
 
-func (e VexillaError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Message, e.Detail)
+func (e InvalidShouldFeatureType) Error() string {
+	return fmt.Sprintf("should only supports toggle, gradual, and selective feature types. Tried type: %s", e.Detail)
 }
 
-func NewVexillaError(errType, detail string) error {
-	messages := map[string]string{
-		"InvalidShouldFeatureType":       "should only supports toggle, gradual, and selective feature types. Tried type: %s",
-		"InvalidShouldFeatureValueType":  "should selective features use the instance_id which is a &str. tried type: %s. consider using should_custom instead.",
-		"InvalidShouldCustomFeatureType": "should_custom only supports toggle, gradual, and selective feature types. Tried type: %s",
-		"InvalidShouldCustomStr":         "should_custom_str only allows &str for the custom_id. the type passed in does not match the value type of the feature. tried type: %s",
-		"InvalidShouldCustomInt":         "should_custom_int only allows i64 for the custom_id. the type passed in does not match the value type of the feature. tried type: %s",
-		"InvalidShouldCustomFloat":       "should_custom_float only allows f64 for the custom_id. the type passed in does not match the value type of the feature. tried type: %s",
-		"InvalidValueFeatureType":        "value only supports the value feature type. tried type: %s",
-		"InvalidValueStringType":         "value_str only supports a string value. tried type: %s",
-		"InvalidValueI64Type":            "value_i64 only supports an i64 value. tried type: %s",
-		"InvalidValueF64Type":            "value_f64 only supports an f64 value. tried type: %s",
-		"InvalidSchedule":                "error parsing schedule: %s",
-		"GroupLookupKeyNotFound":         "could not get key on group_lookup_table",
-		"FlagLookupKeyNotFound":          "could not get key on group_lookup_table",
-		"FlagGroupKeyNotFound":           "could not get key on group_lookup_table",
-		"EnvironmentLookupKeyNotFound":   "could not get key on group_lookup_table",
-		"EnvironmentFeatureKeyNotFound":  "could not get key on group_lookup_table",
-		"UnknownVexillaError":            "unknown vexilla error",
-	}
+func NewInvalidShouldFeatureTypeError(detail string) error {
+	return &InvalidShouldFeatureType{Detail: detail}
+}
 
-	if msg, ok := messages[errType]; ok {
-		return fmt.Errorf(msg, detail)
-	}
+type InvalidShouldFeatureValueType struct {
+	Detail string
+}
 
-	return fmt.Errorf("unknown error type: %s", errType)
+func (e InvalidShouldFeatureValueType) Error() string {
+	return fmt.Sprintf("should selective features use the instance_id which is a &str. tried type: %s. consider using should_custom instead", e.Detail)
+}
+
+func NewInvalidShouldFeatureValueTypeError(detail string) error {
+	return &InvalidShouldFeatureValueType{Detail: detail}
+}
+
+type InvalidShouldCustomGradualFeatureValueType struct {
+	Detail string
+}
+
+func (e InvalidShouldCustomGradualFeatureValueType) Error() string {
+	return fmt.Sprintf("should_custom gradual features only allow &str. tried type: %s.", e.Detail)
+}
+
+func NewInvalidShouldCustomGradualFeatureValueTypeError(detail string) error {
+	return &InvalidShouldCustomGradualFeatureValueType{Detail: detail}
+}
+
+type InvalidShouldCustomStr struct {
+	Detail string
+}
+
+func (e InvalidShouldCustomStr) Error() string {
+	return fmt.Sprintf("should_custom_str only allows &str for the custom_id. the type passed in does not match the value type of the feature. tried type: %s", e.Detail)
+}
+
+func NewInvalidShouldCustomStrError(detail string) error {
+	return &InvalidShouldCustomStr{Detail: detail}
+}
+
+type InvalidShouldCustomInt struct {
+	Detail string
+}
+
+func (e InvalidShouldCustomInt) Error() string {
+	return fmt.Sprintf("should_custom_int only allows i64 for the custom_id. the type passed in does not match the value type of the feature. tried type: %s", e.Detail)
+}
+
+func NewInvalidShouldCustomIntError(detail string) error {
+	return &InvalidShouldCustomInt{Detail: detail}
+}
+
+type InvalidShouldCustomFloat struct {
+	Detail string
+}
+
+func (e InvalidShouldCustomFloat) Error() string {
+	return fmt.Sprintf("should_custom_float only allows f64 for the custom_id. the type passed in does not match the value type of the feature. tried type: %s", e.Detail)
+}
+
+func NewInvalidShouldCustomFloatError(detail string) error {
+	return &InvalidShouldCustomFloat{Detail: detail}
+}
+
+type InvalidValueFeatureType struct {
+	Detail string
+}
+
+func (e InvalidValueFeatureType) Error() string {
+	return fmt.Sprintf("value only supports the value feature type. tried type: %s", e.Detail)
+}
+
+func NewInvalidValueFeatureTypeError(detail string) error {
+	return &InvalidValueFeatureType{Detail: detail}
+}
+
+type InvalidValueStringType struct {
+	Detail string
+}
+
+func (e InvalidValueStringType) Error() string {
+	return fmt.Sprintf("value_str only supports a string value. tried type: %s", e.Detail)
+}
+
+func NewInvalidValueStringTypeError(detail string) error {
+	return &InvalidValueStringType{Detail: detail}
+}
+
+type InvalidValueI64Type struct {
+	Detail string
+}
+
+func (e InvalidValueI64Type) Error() string {
+	return fmt.Sprintf("value_i64 only supports an i64 value. tried type: %s", e.Detail)
+}
+
+func NewInvalidValueI64TypeError(detail string) error {
+	return &InvalidValueI64Type{Detail: detail}
+}
+
+type InvalidValueF64Type struct {
+	Detail string
+}
+
+func (e InvalidValueF64Type) Error() string {
+	return fmt.Sprintf("value_f64 only supports an f64 value. tried type: %s", e.Detail)
+}
+
+func NewInvalidValueF64TypeError(detail string) error {
+	return &InvalidValueF64Type{Detail: detail}
+}
+
+type InvalidSchedule struct {
+	Detail string
+}
+
+func (e InvalidSchedule) Error() string {
+	return fmt.Sprintf("error parsing schedule: %s", e.Detail)
+}
+
+func NewInvalidScheduleError(detail string) error {
+	return &InvalidSchedule{Detail: detail}
+}
+
+type GroupLookupKeyNotFound struct {
+	Detail string
+}
+
+func (e GroupLookupKeyNotFound) Error() string {
+	return fmt.Sprintf("could not get key on group_lookup_table")
+}
+
+func NewGroupLookupKeyNotFoundError(detail string) error {
+	return &GroupLookupKeyNotFound{Detail: detail}
+}
+
+type FlagLookupKeyNotFound struct {
+	Detail string
+}
+
+func (e FlagLookupKeyNotFound) Error() string {
+	return fmt.Sprintf("could not get key on group_lookup_table")
+}
+
+func NewFlagLookupKeyNotFoundError(detail string) error {
+	return &FlagLookupKeyNotFound{Detail: detail}
+}
+
+type FlagGroupKeyNotFound struct {
+	Detail string
+}
+
+func (e FlagGroupKeyNotFound) Error() string {
+	return fmt.Sprintf("could not get key on group_lookup_table")
+}
+
+func NewFlagGroupKeyNotFoundError(detail string) error {
+	return &FlagGroupKeyNotFound{Detail: detail}
+}
+
+type EnvironmentLookupKeyNotFound struct {
+	Detail string
+}
+
+func (e EnvironmentLookupKeyNotFound) Error() string {
+	return fmt.Sprintf("could not get key on group_lookup_table")
+}
+
+func NewEnvironmentLookupKeyNotFoundError(detail string) error {
+	return &EnvironmentLookupKeyNotFound{Detail: detail}
+}
+
+type EnvironmentFeatureKeyNotFound struct {
+	Detail string
+}
+
+func (e EnvironmentFeatureKeyNotFound) Error() string {
+	return fmt.Sprintf("could not get key on group_lookup_table")
+}
+
+func NewEnvironmentFeatureKeyNotFoundError(detail string) error {
+	return &EnvironmentFeatureKeyNotFound{Detail: detail}
+}
+
+type UnknownVexillaError struct {
+	Detail string
+}
+
+func (e UnknownVexillaError) Error() string {
+	return fmt.Sprintf("unknown vexilla error")
+}
+
+func NewUnknownVexillaError(detail string) error {
+	return &UnknownVexillaError{Detail: detail}
 }
--- clients/go/pkg/internal/exceptions_test.go
+++ clients/go/pkg/internal/exceptions_test.go
@@ -1,33 +1,52 @@
 package internal
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestInvalidShouldFeatureType(t *testing.T) {
-	errType := "InvalidShouldFeatureType"
 	detail := "numeric"
 	expectedMessage := "should only supports toggle, gradual, and selective feature types. Tried type: numeric"
 
-	err := NewVexillaError(errType, detail)
+	err := NewInvalidShouldFeatureTypeError(detail)
 
 	if err == nil {
-		t.Fatalf("Expected an error to be returned, got nil")
+		t.Errorf("Expected error to be not nil")
 	}
 
 	if err.Error() != expectedMessage {
 		t.Errorf("Expected error message to be '%s', got '%s'", expectedMessage, err.Error())
+	}
+
+	// Should not be of type InvalidShouldFeatureValueType
+	_, ok := err.(*InvalidShouldFeatureType)
+	if !ok {
+		t.Errorf("Expected error type to be '*InvalidShouldFeatureType', got '%T'", err)
+	}
+
+	// Should not be of type InvalidShouldFeatureValueType
+	_, ok2 := err.(*InvalidShouldFeatureValueType)
+	if ok2 {
+		t.Errorf("Expected error type to be '*InvalidShouldFeatureType', got '*InvalidShouldFeatureValueType'")
 	}
 }
 
 func TestInvalidShouldFeatureValueType(t *testing.T) {
-	expectedMessage := "should selective features use the instance_id which is a &str. tried type: numeric. consider using should_custom instead."
+	detail := "numeric"
+	expectedMessage := "should selective features use the instance_id which is a &str. tried type: numeric. consider using should_custom instead"
 
-	err := NewVexillaError("InvalidShouldFeatureValueType", "numeric")
+	err := NewInvalidShouldFeatureValueTypeError(detail)
 
 	if err == nil {
-		t.Fatalf("Expected an error to be returned, got nil")
+		t.Errorf("Expected error to be not nil")
 	}
 
 	if err.Error() != expectedMessage {
 		t.Errorf("Expected error message to be '%s', got '%s'", expectedMessage, err.Error())
+	}
+
+	_, ok := err.(*InvalidShouldFeatureValueType)
+	if !ok {
+		t.Errorf("Expected error type to be '*InvalidShouldFeatureValueType', got '%T'", err)
 	}
 }
