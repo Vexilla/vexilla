309c6c32b4ca888cff1ec92ddeebb37e5ed80651
Chris Griffing <cmgriffing@gmail.com>
2024-05-18 16:06:44 -0700

    feat(docs/blog): add blog post about hash distribution and algorithms

--- apps/docs/markdoc.config.mjs
+++ apps/docs/markdoc.config.mjs
@@ -20,11 +20,58 @@ export default defineMarkdocConfig({
         },
       },
     },
+    boxplot: {
+      render: component("./src/components/blog/BoxPlot.astro"),
+      attributes: {
+        title: {
+          type: String,
+          default: "",
+          required: false,
+          errorLevel: "critical",
+        },
+        dataPath: {
+          type: String,
+          default: "",
+          required: true,
+          errorLevel: "critical",
+        },
+        count: {
+          type: Number,
+          default: 10,
+          required: true,
+          errorLevel: "critical",
+        },
+        iterations: {
+          type: Number,
+          default: 10,
+          required: false,
+          errorLevel: "critical",
+        },
+        valueKey: {
+          type: String,
+          default: "value",
+          required: false,
+          errorLevel: "critical",
+        },
+        lowerBound: {
+          type: Number,
+          default: 0,
+          required: false,
+          errorLevel: "critical",
+        },
+        upperBound: {
+          type: Number,
+          default: 1,
+          required: false,
+          errorLevel: "critical",
+        },
+      },
+    },
   },
   extends: [
     shiki({
       // Choose from Shiki's built-in themes (or add your own)
-      Default: "github-dark",
+      // Default: "github-dark",
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       // theme: "dracula",
       // Enable word wrap to prevent horizontal scrolling
