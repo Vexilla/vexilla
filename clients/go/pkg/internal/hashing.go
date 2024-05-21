8c09646739f290e5857bbef21012ccf6b187054a
Chris Griffing <cmgriffing@gmail.com>
2024-05-20 18:00:29 -0700

    feat(clients/go): refactor hashing algorithm to FNV-1a

--- clients/go/pkg/internal/hashing.go
+++ clients/go/pkg/internal/hashing.go
@@ -1,21 +1,35 @@
 package internal
 
 import (
-	"math"
+	"hash/fnv"
 	"strconv"
 )
 
+const FNV32_OFFSET_BASIS = 2166136261
+const FNV32_PRIME = 16777619
+
+// export function hashString(stringToHash: string, seed: number) {
+
+//   let total = FNV32_OFFSET_BASIS;
+//   const length = byteArray.length;
+
+//   for (let i = 0; i < length; i++) {
+//     const byte = byteArray[i];
+//     total = total ^ byte;
+//     total = total * FNV32_PRIME;
+//   }
+
+//   const result = ((total * seed) % 100) / 100;
+//   return Math.abs(result);
+// }
+
 func HashStringInstanceID(instanceID string, seed float64) float64 {
 	chars := ([]byte)(instanceID)
+	hash := fnv.New64a()
+	hash.Write(chars)
+	result := hash.Sum64()
 
-	total := 0
-	for _, char := range chars {
-		total += (int)(char)
-	}
-
-	base := float64(total) * seed * 42.0
-
-	return float64(int64(math.Trunc(float64(base)))%100) / 100
+	return float64(uint64(float64(result)*seed)%100) / 100.0
 }
 
 func HashIntInstanceID(instanceId int64, seed float64) float64 {
--- clients/go/pkg/internal/hashing_test.go
+++ clients/go/pkg/internal/hashing_test.go
@@ -8,7 +8,7 @@ func TestHashing(tester *testing.T) {
 
 	uuid := "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a"
 
-	workingGradual := float64(0.11)
+	workingGradual := float64(0.32)
 	nonWorkingGradual := float64(0.22)
 
 	shouldWorkValue := HashStringInstanceID(uuid, workingGradual)
