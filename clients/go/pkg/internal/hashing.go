0beab1ad7f0a79a8f8c0ad520860dece479f8fd4
Chris Griffing <cmgriffing@gmail.com>
2024-05-21 18:50:12 -0700

    feat(clients/go): get tests passing for go client gradual hashing

--- clients/go/pkg/internal/hashing.go
+++ clients/go/pkg/internal/hashing.go
@@ -5,31 +5,16 @@ import (
 	"strconv"
 )
 
-const FNV32_OFFSET_BASIS = 2166136261
-const FNV32_PRIME = 16777619
-
-// export function hashString(stringToHash: string, seed: number) {
-
-//   let total = FNV32_OFFSET_BASIS;
-//   const length = byteArray.length;
-
-//   for (let i = 0; i < length; i++) {
-//     const byte = byteArray[i];
-//     total = total ^ byte;
-//     total = total * FNV32_PRIME;
-//   }
-
-//   const result = ((total * seed) % 100) / 100;
-//   return Math.abs(result);
-// }
+// const FNV32_OFFSET_BASIS = 2166136261
+// const FNV32_PRIME = 16777619
 
 func HashStringInstanceID(instanceID string, seed float64) float64 {
 	chars := ([]byte)(instanceID)
-	hash := fnv.New64a()
+	hash := fnv.New32a()
 	hash.Write(chars)
-	result := hash.Sum64()
+	result := hash.Sum32()
 
-	return float64(uint64(float64(result)*seed)%100) / 100.0
+	return float64(uint64(float64(result)*seed)%1000) / 1000.0
 }
 
 func HashIntInstanceID(instanceId int64, seed float64) float64 {
