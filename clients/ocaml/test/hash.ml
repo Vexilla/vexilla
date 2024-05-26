e3068e679730c5032b58acd7ba280d6cd62b47b6
Chris Griffing <cmgriffing@gmail.com>
2024-05-26 14:26:51 -0700

    feat(clients/ocaml): switch to fnv-1a and get tests passing for ocaml client

--- clients/ocaml/test/hash.ml
+++ clients/ocaml/test/hash.ml
@@ -8,23 +8,23 @@ let () =
       ( "hash",
         [
           test_case "should" `Quick (fun () ->
-              let seed = 0.11 in
+              let seed = 0.32 in
               let actual =
                 Hash.hash_instance_id ~seed
                   "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a"
               in
-              let expected = 0.28 in
+              let expected = 0.19 in
 
-              check bool "equal" true (actual = expected));
+              (check @@ float epsilon_float) "equal" expected actual);
           test_case "should not" `Quick (fun () ->
               let seed = 0.22 in
               let actual =
                 Hash.hash_instance_id ~seed
                   "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a"
               in
-              let expected = 0.56 in
+              let expected = 0.943 in
 
-              check bool "equal" true (actual = expected));
+              (check @@ float epsilon_float) "equal" expected actual);
         ] );
     ]
 ;;
