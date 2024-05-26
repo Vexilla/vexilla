9ad6bda0c6266b8d82cc55dfec34efb75dcff798
Chris Griffing <cmgriffing@gmail.com>
2024-05-26 16:30:06 -0700

    fix(clients): bump version numbers of clients

--- clients/elixir/mix.exs
+++ clients/elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule VexillaClient.MixProject do
   def project do
     [
       app: :vexilla_client_elixir,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
